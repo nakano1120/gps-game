e8f948a44f483def580ab06f1f9702b9a36948c8
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2020-01-21 16:02:37 +0900

    Add files via upload

--- yubin_script.js
+++ yubin_script.js
@@ -15,7 +15,7 @@ var taro = new Vue({
       getaddress:function(){
         if(this.postnum>1000000 && this.postnum<9999999){
           axios
-            .get('//zip-cloud.appspot.com/api/search?zipcode='+this.postnum, {headers : this.headers})
+            .get('//zip-cloud.appspot.com/api/search?zipcode='+this.postnum, this.headers)
             .then(response => (address = response.results))
         }
       }

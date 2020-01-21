be0c2e7f8436b2eb3985f3b749edae24974dc080
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2020-01-21 12:59:09 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -191,7 +191,7 @@ var taro = new Vue({
       getapi:function(la,lo){
         axios
           .get('//www.finds.jp/ws/rgeocode.php?lat='+la+'&lon='+lo, this.headers)
-          .then(response => (this.address = response))
+          .then(response => (this.address = response.result[0])
         console.log(this.address)
         if(first==1){
           if(this.address.local != null){

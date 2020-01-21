ae155347c8ee9c6026760576de12898ea9cd862c
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2020-01-21 16:09:28 +0900

    Add files via upload

--- yubin_script.js
+++ yubin_script.js
@@ -8,7 +8,8 @@ var taro = new Vue({
       headers: { 
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Methods': '*',
-        'Access-Control-Allow-Headers': '*'
+        'Access-Control-Allow-Headers': '*',
+        "Access-Control-Allow-Headers":["X-Requested-With","Origin", "X-Csrftoken", "Content-Type", "Accept"]
       }
     },
     methods:{
@@ -16,7 +17,7 @@ var taro = new Vue({
         if(this.postnum>1000000 && this.postnum<9999999){
           axios
             .get('//zip-cloud.appspot.com/api/search?zipcode='+this.postnum, this.headers)
-            .then(response => (address = response))
+            .then(response => (address = response.results))
         }
       }
     }

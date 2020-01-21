a298e6a6d248a132a3beaae4924970b2a167fcd1
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2020-01-21 15:50:38 +0900

    Add files via upload

---
+++ yubin_script.js
@@ -0,0 +1,30 @@
+
+var taro = new Vue({
+    el: "#app",
+    data: {
+      postnum:"",
+      address:[
+        {
+          results:{
+            address1:"",
+            address2:"",
+            address3:""
+          }
+        }
+      ],
+      headers: { 
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*'
+      }
+    },
+    methods:{
+      getaddress:function(){
+        if(this.postnum>1000000 && this.postnum<9999999){
+          axios
+            .get('https://zip-cloud.appspot.com/api/search?zipcode='+this.postnum, this.headers)
+            .then(response => (this.address = response.results))
+        }
+      }
+    }
+})
